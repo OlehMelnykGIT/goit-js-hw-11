25e8913f9263bb91177c246e664b29c5a2f0edeb
oleh <melniko986@gmail.com>
2026-09-18 22:59:24 +0200

    Move main script to src root

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
-import { getImagesByQuery } from './pixabay-api';
-import { clearGallery, createGallery, hideLoader, showLoader } from './render-functions';
+import { getImagesByQuery } from './js/pixabay-api';
+import { clearGallery, createGallery, hideLoader, showLoader } from './js/render-functions';
 
 const form = document.querySelector('.form');
 const searchInput = form.elements['search-text'];
